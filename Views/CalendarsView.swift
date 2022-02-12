f5799c3bc1e6fb14773b562bee8bb60d75727e2e
Maria Obregon Garcia <mobregong@gmail.com>
2022-02-12 16:48:24 -0500

    Adds CalendarsView UI

--- Views/CalendarsView.swift
+++ Views/CalendarsView.swift
@@ -11,6 +11,7 @@ struct CalendarsView: View {
     
     @State var isConnectMode = true
     @State var calendarLink = ""
+    @State var calendarName = ""
     
     var body: some View {
         NavigationView {
@@ -19,31 +20,65 @@ struct CalendarsView: View {
                     Picker(selection: $isConnectMode, label: Text("Picker here")) {
                         Text("Connect")
                             .tag(true)
-                        Text("Settings")
+                        Text("Connected")
                             .tag(false)
                     }.pickerStyle(SegmentedPickerStyle())
-                    
-                    Text("New Calendar")
-                        .font(.system(size:28, weight: .semibold))
-//                            .font(.system(Font.TextStyle))
-//                            .padding(.trailing, 100)
-//                            style="text-align:left"
-//                        .foregroundColor(.blue)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    
-                    HStack {
-                        TextField("Calendar link", text: $calendarLink)
-                            .autocapitalization(.none)
-                            .padding(12)
-                            .background(Color(.init(white: 0, alpha: 0.05)))
-                        Button {
-                            handleAction ()
-                        } label: {
-                            Text("⏎")
-                                .foregroundColor(.white)
+
+                    if isConnectMode {
+                        Text("Apple Calendar")
+                            .font(.system(size:24, weight: .semibold))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        HStack {
+                            TextField("Calendar name", text: $calendarName)
+                                .autocapitalization(.none)
                                 .padding(12)
-                                .font(.system(size:14, weight: .semibold))
-                        }.background(Color.blue)
+                                .background(Color(.init(white: 0, alpha: 0.05)))
+                            Button {
+                                handleAppleCal ()
+                            } label: {
+                                Text("⏎")
+                                    .foregroundColor(.white)
+                                    .padding(12)
+                                    .font(.system(size:14, weight: .semibold))
+                            }.background(Color.blue)
+                        }
+                        
+                        Text("Google Calendar")
+                            .font(.system(size:24, weight: .semibold))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        HStack {
+                            TextField("Calendar link", text: $calendarLink)
+                                .autocapitalization(.none)
+                                .padding(12)
+                                .background(Color(.init(white: 0, alpha: 0.05)))
+                            Button {
+                                handleGoogleCal ()
+                            } label: {
+                                Text("⏎")
+                                    .foregroundColor(.white)
+                                    .padding(12)
+                                    .font(.system(size:14, weight: .semibold))
+                            }.background(Color.blue)
+                        }
+                    } else {
+                        Text("Currently connected to:")
+                            .font(.system(size:24, weight: .semibold))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        HStack {
+                            Text("Calendar name")
+                                .font(.system(size:20, weight: .light))
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            Button {
+                                handleRefresh ()
+                            } label: {
+                                Text("Refresh")
+                                    .foregroundColor(.white)
+                                    .padding(12)
+                                    .font(.system(size:14, weight: .semibold))
+                            }.background(Color.blue)
+                        }
                     }
                 }.padding()
             }
@@ -52,8 +87,14 @@ struct CalendarsView: View {
         }
         .navigationViewStyle(StackNavigationViewStyle())
     }
-    private func handleAction () {
-        print("Should register new calendar link")
+    private func handleAppleCal () {
+        print("Should register new Apple calendar")
+    }
+    private func handleGoogleCal () {
+        print("Should register new Google calendar link")
+    }
+    private func handleRefresh () {
+        print("Should refresh calendar connection")
     }
 }
 
