fcf7047cec6a8c565817cd8ba15b71b8c9658d6e
Maria Obregon Garcia <mobregong@gmail.com>
2022-02-13 20:05:38 -0500

    Adds working API call to calendar connect button (parses calendar and stores events in firestore)

--- Views/CalendarsView.swift
+++ Views/CalendarsView.swift
@@ -12,6 +12,12 @@ struct CalendarsView: View {
     @State var isConnectMode = true
     @State var calendarLink = ""
     @State var calendarName = ""
+    @State var calendarConnectMessage = ""
+    
+//    @Published var selectedCalendars: Set<EKCalendar>?
+
+    
+//    private var cc = CalendarChooser(calendars: self.$eventsRepository.selectedCalendars, eventStore: self.eventsRepository.eventStore)
     
     var body: some View {
         NavigationView {
@@ -92,7 +98,38 @@ struct CalendarsView: View {
     }
     private func handleGoogleCal () {
         print("Should register new Google calendar link")
+
+//        Get current user id
+        guard let uid = FirebaseManager.shared.auth.currentUser?.uid else { return }
+        
+// //     API call
+//        Prepare json data which will be our request body (uid and url)
+        let json = ["uid": uid,"url": self.calendarLink]
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        
+//        post request
+        let url = URL(string: "http://127.0.0.1:5000/firestore/events")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+
+        // insert json data to the request
+        request.httpBody = jsonData
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                print(error?.localizedDescription ?? "No data")
+                return
+            }
+            let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
+            if let responseJSON = responseJSON as? [String: Any] {
+                print(responseJSON)
+            }
+        }
+        task.resume()
+//        do not currently have an error message
     }
+        
     private func handleRefresh () {
         print("Should refresh calendar connection")
     }
