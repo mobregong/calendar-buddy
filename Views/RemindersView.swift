3d7606e08214278fa9b9af50468f64c124d9ed01
Maria Obregon Garcia <mobregong@gmail.com>
2022-02-15 01:37:35 -0500

    Adds button to create a date/time-based notification

--- Views/RemindersView.swift
+++ Views/RemindersView.swift
@@ -22,7 +22,7 @@ struct RemindersView: View {
                     }
                 }
             }
-            Button("Schedule Notification") {
+            Button("Schedule Notification in 5 seconds") {
                 let content = UNMutableNotificationContent()
                 content.title = "Feed the cat"
                 content.subtitle = "It looks hungry"
@@ -36,7 +36,29 @@ struct RemindersView: View {
 
                 // add our notification request
                 UNUserNotificationCenter.current().add(request)
+            }
+            Button("Schedule Notification for a certain date") {
+                let content = UNMutableNotificationContent()
+                content.title = "Feed the cat"
+                content.subtitle = "It looks hungry"
+                content.sound = UNNotificationSound.default
+                
+                // specify data
+                var dateInfo = DateComponents()
+                dateInfo.day = 15
+                dateInfo.month = 02
+                dateInfo.year = 2022
+                dateInfo.hour = 01
+                dateInfo.minute = 26
 
+                // show this notification five seconds from now
+                let trigger = UNCalendarNotificationTrigger(dateMatching: dateInfo, repeats: false)
+
+                // choose a random identifier
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+
+                // add our notification request
+                UNUserNotificationCenter.current().add(request)
             }
         }
     }
