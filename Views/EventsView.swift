20dd90966ce766585c8a4ced13ebf309ffaacac0
Maria Obregon Garcia <mobregong@gmail.com>
2022-02-15 18:42:33 +0000

    Implements get today's events

--- Views/EventsView.swift
+++ Views/EventsView.swift
@@ -15,7 +15,10 @@ struct EventsView: View {
 //    }
 //    @State private var EventMode: Event = .next
     @State var isTodayMode = true
+    @State var count = 0
     @State var todayResponse = ""
+    @State var todayTimes = ""
+
     
     
     var body: some View {
@@ -43,7 +46,9 @@ struct EventsView: View {
                                 .font(.system(size:14, weight: .semibold))
                         }.background(Color.blue)
                         
-                        Text(todayResponse)
+                        Text(self.todayResponse)
+                        Text(self.todayTimes)
+                        
                     } else {
                         Button {
                            fetchNext()
@@ -67,8 +72,43 @@ struct EventsView: View {
     private func fetchNext () {
         print("fetch next")
     }
+    
+    
+    
     private func fetchToday () {
-        print("fetch today")
+        setToday()
+        guard let uid = FirebaseManager.shared.auth.currentUser?.uid else { return }
+        
+        FirebaseManager.shared.firestore.collection("users").document(uid).collection("user_info").document("events").collection("events_today").getDocuments(){ (querySnapshot, err) in
+                if let err = err {
+                    print("Error getting documents: \(err)")
+                } else {
+                    for document in querySnapshot!.documents {
+//                        print("\(document.documentID) => \(document.data())")
+//                        print("\(document.data())")
+//                        self.todayResponse += ("\(document.data())")
+//                        print(type(of: self.todayResponse))
+                        
+                        let data = document.data()
+                        let name = data["event_name"]
+                        let time = data["event_time"]
+//                        let description = data["description"]
+//                        let location = data["location"]
+                        
+                        self.count += 1
+                        self.todayResponse.append(" \(name as! String)")
+//                        print(self.todayResponse)
+//                        print("Description: \(description)")
+                        self.todayTimes.append(" \(time as! String)")
+//                        print(self.todayResponse)
+//                        print("Description: \(description)")
+                    }
+                    print(self.count)
+                }
+            }
+    }
+    private func setToday () {
+        print("set today")
         //        Get current user id
         guard let uid = FirebaseManager.shared.auth.currentUser?.uid else { return }
         
@@ -80,13 +120,14 @@ struct EventsView: View {
 //        post request
         let url = URL(string: "http://127.0.0.1:5000/firestore/events/today")!
         var request = URLRequest(url: url)
-        request.httpMethod = "GET"
+        request.httpMethod = "POST"
 
         // insert json data to the request
         request.httpBody = jsonData
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
 
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        let task = URLSession.shared.dataTask(with: request)
+        { data, response, error in
             guard let data = data, error == nil else {
                 //        do not currently have working error message
                 print(error?.localizedDescription ?? "No data")
@@ -95,9 +136,6 @@ struct EventsView: View {
             let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
             if let responseJSON = responseJSON as? [String: Any] {
                 print(responseJSON)
-            }
-            struct BlogPost: Decodable {
-                let title: String
             }
         }
         task.resume()
